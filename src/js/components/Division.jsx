18ee866e40c6ab5ce826e81972b6e2763af73c40
dualss <dariongduals@gmail.com>
2025-02-21 20:04:03 +0000

    Arreglo en Nuevas Tareas

--- src/js/components/Division.jsx
+++ src/js/components/Division.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -9,61 +9,61 @@ import ListaTareas from "./Listatareas";
 
 
 function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
 }
 
 CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
 export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} centered variant="scrollable"
-  scrollButtons="auto"
-  aria-label="scrollable auto tabs example">
-          <Tab label="Control de Usuarios" {...a11yProps(0)} />
-          <Tab label="Envio de Tareas" {...a11yProps(1)} />
-          <Tab label="Tareas pendientes" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        <CrearUsuarios/>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <NuevasTareas/>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <ListaTareas/>
-      </CustomTabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} variant="scrollable"
+                    scrollButtons="auto"
+                    aria-label="scrollable auto tabs example">
+                    <Tab label="Control de Usuarios" {...a11yProps(0)} />
+                    <Tab label="Envio de Tareas" {...a11yProps(1)} />
+                    <Tab label="Tareas pendientes" {...a11yProps(2)} />
+                </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+                <CrearUsuarios />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+                <NuevasTareas />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+                <ListaTareas />
+            </CustomTabPanel>
+        </Box>
+    );
 }
--- src/js/components/NuevasTareas.jsx
+++ src/js/components/NuevasTareas.jsx
@@ -42,7 +42,7 @@ const NuevasTareas = () => {
     
     return (
             <div className="container pt-3">
-            <h2>TAREAS PARA UN USUARIO</h2>
+            <h2>Tareas para el usuario</h2>
             {alertaEnvio.open && (
                 <Stack sx={{ width: '100%' }} spacing={2}>
                     <Alert severity={alertaEnvio.severity}>
@@ -51,13 +51,13 @@ const NuevasTareas = () => {
                 </Stack>
             )}
             <form onSubmit={envioFormulario}>
-                <label>Nombre de usuario:</label>
-                <input className="m-2"type="text"
+                <label className="mt-2">Nombre de usuario:</label>
+                <input className="m-3"type="text"
                     placeholder="Nombre del usuario"
                     value={user}
                     onChange={(e) => setUser(e.target.value)} ></input>
-                <label>Nueva tarea:</label>
-                <input className="m-2" type="text" placeholder="Tarea nueva" value={tarea} onChange={(t) => setTarea(t.target.value)}/>
+                <label className="">Nueva tarea:</label>
+                <input className="m-3" type="text" placeholder="Tarea nueva" value={tarea} onChange={(t) => setTarea(t.target.value)}/>
                 <Button variant="contained" type="submit">Enviar tarea</Button>
             </form>
             </div>
